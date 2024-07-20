70e3b90aa5959d7bff9339ef06d9e6f558ed91b3
VikramadityaKhupse <2022bit052@sggs.ac.in>
2024-07-20 10:28:19 +0530

    hackerrank question merge the tools

---
+++ PythonCodes/merge_the_tools.py
@@ -0,0 +1,13 @@
+# Answer for question: https://www.hackerrank.com/challenges/merge-the-tools/problem?isFullScreen=true
+
+def merge_the_tools(string, k):
+    substring1 = ""
+    for i in range(0,len(string),k):
+        substring = string[i:k+i]
+        print("".join(list(set(substring))))
+        
+    # your code goes here
+
+if __name__ == '__main__':
+    string, k = input(), int(input())
+    merge_the_tools(string, k)
