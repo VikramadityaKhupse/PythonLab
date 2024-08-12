203a140a63691c2a1895d9446d514af4f576504f
VikramadityaKhupse <2022bit052@sggs.ac.in>
2024-08-12 23:25:05 +0530

    updated mod funtion

--- mod.py
+++ mod.py
@@ -1,10 +1,9 @@
-       
 def modulo(num1, num2):
-	if num2 == 0:
-        	raise ValueError("Division by zero is undefined.")
-	else:
-		quo = num1 // num2
-		return num1 - quo * num2
-		
+    if num2 == 0:
+        raise ValueError("Division by zero is undefined.")
+    else:
+        quo = num1 // num2
+        return num1 - quo * num2
 
-print(modulo2(15, -4))
+
+print(modulo(15, -4))
