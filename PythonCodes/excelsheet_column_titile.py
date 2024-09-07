5c7e784d41d3e705b7f84ad85f377c267372b093
VikramadityaKhupse <2022bit052@sggs.ac.in>
2024-09-07 21:35:26 +0530

    Given an integer columnNumber, return its corresponding column title as it appears in an Excel sheet.

---
+++ PythonCodes/excelsheet_column_titile.py
@@ -0,0 +1,12 @@
+# https://leetcode.com/problems/excel-sheet-column-title/?envType=problem-list-v2&envId=string
+class Solution:
+    def convertToTitle(self, columnNumber: int) -> str:
+        result = []
+    
+        while columnNumber > 0:
+            columnNumber -= 1
+            remainder = columnNumber % 26
+            result.append(chr(remainder + ord('A')))
+            columnNumber //= 26
+    
+        return ''.join(result[::-1]) 
