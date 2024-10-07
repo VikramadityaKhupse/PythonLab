42d7713ffa81c6ae23acf78543cc8b15f92c02e3
VikramadityaKhupse <2022bit052@sggs.ac.in>
2024-10-07 09:40:18 +0530

    Added percentage

--- checkPerformance.py
+++ checkPerformance.py
@@ -3,8 +3,8 @@ import time
 
 def checkPerformance(approaches):
     time_taken = []
-    # test = ["aba", "bbb", (1, 2, 22, 121, "bbb", ["bbb", 121]), []]
-    test = [1, 2, 3, 4, 5, 6, 7]
+    # test = [1, 2, 3, 4, 5, 6, 7]
+    test = [(1, 2, 22, 121, "bbb", ["bbb", 121]), [], "aba", "bbb",]
 
     for approach in approaches:
         start_time = time.time()
@@ -13,7 +13,14 @@ def checkPerformance(approaches):
         end_time = time.time()
         time_taken.append(end_time - start_time)
 
-    return list(zip(approaches, time_taken))
+    first_approach_time = time_taken[0]
+    percentage_faster = []
+    
+    for i in range(1, len(approaches)):
+        percentage_diff = ((first_approach_time - time_taken[i]) / first_approach_time) * 100
+        percentage_faster.append((approaches[i].__name__, percentage_diff))
+    
+    return time_taken, percentage_faster
 
 
 #____________________________________________________________________________________
@@ -83,10 +90,12 @@ def odd_even2(l):
 
 # test = [(1, 2, 22, 121, "bbb", ["bbb", 121]), [], "aba", "bbb",]
 test2 = [1, 2, 3, 4, 5, 6, 7]
-print(odd_even1(test2))
-print(odd_even2(test2))
+# print(odd_even1(test2))
+# print(odd_even2(test2))
 
 # print(f"Approach 1: {approach1(test)}")
 # print(f"Approach 2: {approach2(test)}")
 # print(f"Approach 2: {approach3(test)}")
-print(checkPerformance([odd_even1, odd_even2]))
+time_taken, percentage_faster = checkPerformance([approach1, approach2,approach3])
+print("Execution times (in seconds):", time_taken)
+print("Percentage faster than the first approach:", percentage_faster)
