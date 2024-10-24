a9bcfe5e573c8ace4d1d7a340b18be80b9e582c0
VikramadityaKhupse <2022bit052@sggs.ac.in>
2024-10-24 22:07:01 +0530

    Given an array of distinct integers arr, find all pairs of elements with the minimum absolute difference of any two elements.
    Return a list of pairs in ascending order(with respect to pairs), each pair [a, b] follows
    
        a, b are from arr
        a < b
        b - a equals to the minimum absolute difference of any two elements in arr

---
+++ PythonCodes/min_abs_diff.py
@@ -0,0 +1,16 @@
+# https://leetcode.com/problems/minimum-absolute-difference/
+class Solution:
+    def minimumAbsDifference(self, arr):
+        arr.sort()
+        
+        min_diff = float('inf')
+        for i in range(1, len(arr)):
+            min_diff = min(min_diff, arr[i] - arr[i-1])
+        
+        result = []
+        for i in range(1, len(arr)):
+            if arr[i] - arr[i-1] == min_diff:
+                result.append([arr[i-1], arr[i]])
+        
+        return result
+        
