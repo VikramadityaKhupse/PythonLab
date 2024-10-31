61e6d1e9bc53fe4fe768b7ae230e75d112422259
VikramadityaKhupse <2022bit052@sggs.ac.in>
2024-10-31 09:57:06 +0530

    There is a robot starting at the position (0, 0), the origin, on a 2D plane. Given a sequence of its moves, judge if this robot ends up at (0, 0) after it completes its moves.
    You are given a string moves that represents the move sequence of the robot where moves[i] represents its ith move. Valid moves are 'R' (right), 'L' (left), 'U' (up), and 'D' (down).
    
    Return true if the robot returns to the origin after it finishes all of its moves, or false otherwise.

--- PythonCodes/robot_origin.py
+++ PythonCodes/robot_origin.py
@@ -1,4 +1,4 @@
 # https://leetcode.com/problems/robot-return-to-origin/submissions/1438764223/class Solution:
-    def judgeCircle(self, moves: str) -> bool:
-        return (moves.count('R') == moves.count('L')) and (moves.count('U') == moves.count('D'))
+def judgeCircle(self, moves: str) -> bool:
+    return (moves.count('R') == moves.count('L')) and (moves.count('U') == moves.count('D'))
             
