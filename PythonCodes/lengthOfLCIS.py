e061975b07c447c5dbbc4b6e2172ac0c9633c83b
VikramadityaKhupse <2022bit052@sggs.ac.in>
2024-11-11 15:53:46 +0530

    Given an unsorted array of integers nums, return the length of the longest continuous increasing subsequence (i.e. subarray). The subsequence must be strictly increasing.

---
+++ PythonCodes/lengthOfLCIS.py
@@ -0,0 +1,11 @@
+# https://leetcode.com/problems/longest-continuous-increasing-subsequence/
+class Solution:
+    def findLengthOfLCIS(self, nums: List[int]) -> int:
+        ans = cnt = 1
+        for i, x in enumerate(nums[1:]):
+            if nums[i] < x:
+                cnt += 1
+                ans = max(ans, cnt)
+            else:
+                cnt = 1
+        return ans
