58148bd3fdad99c0cee777b626e6e53a675b6e00
VikramadityaKhupse <2022bit052@sggs.ac.in>
2024-11-16 14:08:25 +0530

    Optimized code, used dictionary instead of lists

--- toRoman.py
+++ toRoman.py
@@ -1,28 +1,28 @@
 def to_roman(value):
-    num = [1, 4, 5, 9, 10, 40, 50, 90,
-        100, 400, 500, 900, 1000]
-    sym = ["I", "IV", "V", "IX", "X", "XL",
-        "L", "XC", "C", "CD", "D", "CM", "M"]
-    i = 12
-    output = ""
+    roman_map = {
+        1000: "M", 900: "CM", 500: "D", 400: "CD", 100: "C",
+        90: "XC", 50: "L", 40: "XL", 10: "X", 9: "IX",
+        5: "V", 4: "IV", 1: "I"
+    }
     
-    if type(value) == str:
+    roman_keys = sorted(roman_map.keys(), reverse=True)
+
+    if isinstance(value, str):
         if value.startswith("0b"):
             value = int(value, 2)
         elif value.startswith("0x"):
             value = int(value, 16)
         elif value.startswith("0o"):
             value = int(value, 8)
-        
-     
-    while value:
-        div = value // num[i]
-        value %= num[i]
- 
-        while div:
-            output += sym[i]
-            div -= 1
-        i -= 1
-    return output
+        else:
+            value = int(value) 
 
-print(to_roman("0xddd"))
+    result = ""
+    for key in roman_keys:
+        while value >= key:
+            result += roman_map[key]
+            value -= key
+
+    return result
+
+print(to_roman("0xddd"))  
