f0f3bf0095455e29d34d8cfbde2855ac34dc7b21
VikramadityaKhupse <2022bit052@sggs.ac.in>
2024-11-22 23:55:03 +0530

    Given a binary string s, return the number of non-empty substrings that have the same number of 0's and 1's, and all the 0's and all the 1's in these substrings are grouped consecutively.
    Substrings that occur multiple times are counted the number of times they occur.

---
+++ PythonCodes/count_binary_substrgins.py
@@ -0,0 +1,19 @@
+# https://leetcode.com/problems/count-binary-substrings/description/
+class Solution:
+    def countBinarySubstrings(self, s: str) -> int:
+        groups = []
+        count = 1
+        
+        for i in range(1, len(s)):
+            if s[i] == s[i - 1]:
+                count += 1
+            else:
+                groups.append(count)
+                count = 1
+        groups.append(count)  
+        
+        result = 0
+        for i in range(len(groups) - 1):
+            result += min(groups[i], groups[i + 1])
+        
+        return result
