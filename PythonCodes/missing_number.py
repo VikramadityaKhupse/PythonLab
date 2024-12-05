0f85707c971f7f0810d7d6da2e3b61e052594b5f
VikramadityaKhupse <2022bit052@sggs.ac.in>
2024-12-05 11:21:37 +0530

    Given an array nums containing n distinct numbers in the range [0, n], return the only number in the range that is missing from the array.

---
+++ PythonCodes/missing_number.py
@@ -0,0 +1,4 @@
+# https://leetcode.com/problems/missing-number/description/
+class Solution:
+    def missingNumber(self, nums: List[int]) -> int:
+        return sum(range(0,len(nums)+1)) - sum(nums)
